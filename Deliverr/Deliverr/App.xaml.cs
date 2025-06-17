97cadf7e297bb055da2901190a5d14cae9199d93
StanM662 <2403308morreau@zuyd.nl>
2025-06-17 10:33:19 +0200

    .NET MAUI basisstructuur toegevoegd

---
+++ Deliverr/Deliverr/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Deliverr
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Deliverr/Deliverr/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Deliverr
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
