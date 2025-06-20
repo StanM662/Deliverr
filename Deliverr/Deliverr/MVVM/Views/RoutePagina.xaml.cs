fc62b28c17c43bf56a43d393eb5a4426765e6581
StanM662 <2403308morreau@zuyd.nl>
2025-06-20 11:07:12 +0200

    Inputvelden toegevoegd aan route pagina

--- Deliverr/Deliverr/MVVM/Views/RoutePagina.xaml.cs
+++ Deliverr/Deliverr/MVVM/Views/RoutePagina.xaml.cs
@@ -1,3 +1,5 @@
+using System.Formats.Tar;
+
 namespace Deliverr;
 
 public partial class RoutePagina : ContentPage
@@ -5,15 +7,45 @@ public partial class RoutePagina : ContentPage
     public RoutePagina()
     {
         InitializeComponent();
-        ToonKaartMetRoute();
+        ToonKaartMetRoute("50.53284, 5.44237", "50.52538, 5.57355");
     }
 
-    private void ToonKaartMetRoute()
+    private void OnToonRouteClicked(object sender, EventArgs e)
     {
-        string latitude = "50.53284, 5.44237";
-        string longitude = "50.52538, 5.57355";
+        string startInput = StartEntry.Text;
+        string endInput = EndEntry.Text;
 
-        string routeCoordinaten = $"[[{latitude}], [{longitude}]]"; 
+        if (TryParseCoordinates(startInput, out double[] startCoords) &&
+            TryParseCoordinates(endInput, out double[] endCoords))
+        {
+            ToonKaartMetRoute(startInput, endInput);
+        }
+        else
+        {
+            DisplayAlert("Fout", "Voer geldige coördinaten in (bijv. 50.53284, 5.44237)", "OK");
+        }
+    }
+
+    private bool TryParseCoordinates(string input, out double[] coords)
+    {
+        coords = null;
+        var parts = input.Split(',');
+        if (parts.Length != 2)
+            return false;
+
+        if (double.TryParse(parts[0].Trim(), out double lat) &&
+            double.TryParse(parts[1].Trim(), out double lon))
+        {
+            coords = new double[] { lat, lon };
+            return true;
+        }
+        return false;
+    }
+
+    private void ToonKaartMetRoute(string start, string end)
+    {
+        string routeCoordinaten = $"[{FormatCoords(start)}, {FormatCoords(end)}]";
+
         string html = $@"
 <!DOCTYPE html>
 <html>
@@ -24,22 +56,24 @@ public partial class RoutePagina : ContentPage
   <link rel='stylesheet' href='https://unpkg.com/leaflet@1.7.1/dist/leaflet.css'/>
 </head>
 <body>
-  <div id='map' style='width:100vw; height:400px;'></div>
+  <div id='map' style='width:100vw; height:100vh;'></div>
   <script src='https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'></script>
   <script>
-    var map = L.map('map').setView([49.417, 8.684], 14);
+    var route = {routeCoordinaten};
+    var map = L.map('map').setView(route[0], 13);
     L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png').addTo(map);
-
-    var route = [[50.53284, 5.44237], [50.52538, 5.57355]];
-    var latlngs = route.map(function(c) {{ return [c[1], c[0]]; }});
-    var route = [[50.53284, 5.44237], [50.52538, 5.57355]];
-    var polyline = L.polyline(route, {{color: 'blue' }}).addTo(map);
+    var polyline = L.polyline(route, {{ color: 'blue' }}).addTo(map);
     map.fitBounds(polyline.getBounds());
-
   </script>
 </body>
 </html>";
 
         KaartView.Source = new HtmlWebViewSource { Html = html };
+    }
+
+    private string FormatCoords(string coord)
+    {
+        var parts = coord.Split(',');
+        return $"[{parts[0].Trim()}, {parts[1].Trim()}]";
     }
 }
